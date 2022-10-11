f9b3118d03d283670c3a38ecd19d10ba45c7d6ce
mdrobiulislamo9 <robiul.rubin@gmail.com>
2022-10-11 23:52:12 +0600

    alhamdullillah 100% complete

--- src/component/blog/Blog.js
+++ src/component/blog/Blog.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
+        <div className='container'>
                 <h1>What is the purpose of react router?</h1>
                 <p>React Router is a standard library for routing in React.
                     It enables the navigation among views of various components 
@@ -21,7 +21,7 @@ const Blog = () => {
                 </p>
            
             
-                <h1>React useRef Hook</h1>
+                <h1>About React useRef Hook.</h1>
                 <p>Essentially, useRef is like a “box” that can hold a mutable value in its .current property.
                    You might be familiar with refs primarily as a way to access the DOM. If you pass a ref
                    object to React with div ref=myRe , React will set its .current property to the
