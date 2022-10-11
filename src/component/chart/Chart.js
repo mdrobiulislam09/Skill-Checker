2d81f6de77e61144ce784bec37c45c7008cd9343
mdrobiulislamo9 <robiul.rubin@gmail.com>
2022-10-11 21:05:43 +0600

    chart responsive done

--- src/component/chart/Chart.js
+++ src/component/chart/Chart.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Line, LineChart, ResponsiveContainer, XAxis,  YAxis, Tooltip} from 'recharts';
+import './Chart.css'
 
 const Chart = () => {
     const datas = useLoaderData()
     const {data} = datas ;
     return (
-        <div className='container p-5'>
+        <div className='container mt-5'>
             {
-            <LineChart width={300} height={400} data={data}>
-                <XAxis dataKey="name" />
-                <YAxis/>
-                <Tooltip />
-                <Line type="monotone" dataKey="total" stroke="blueviolet"></Line>
-            </LineChart>
+            <ResponsiveContainer width="90%" height={500}>
+                <LineChart data={data}>
+                    <XAxis dataKey="name" />
+                    <YAxis/>
+                    <Tooltip />
+                    <Line type="monotone" dataKey="total" stroke="blueviolet"></Line>
+                </LineChart>
+            </ResponsiveContainer>
             }
         </div>
     );
--- src/component/subject/Subject.js
+++ src/component/subject/Subject.js
@@ -8,7 +8,7 @@ const Subject = () => {
     const {data} = subjects ;
     return (
         <div className='skill-body text-warning'>
-            <div className='header p-2'>
+            <div className='header p-3'>
                 <h1 className='m-0'>Introduction</h1>
                 <p className='m-0 '>Expert judgement has been frequently employed with reading assessments to gauge the skills potentially measured by test tasks, for purposes such as construct validation or producing diagnostic information. There was a bigger gap between them regarding the specific skills involved in each task.</p>
             </div>
